8632abb19b18ef10b8da1f5cfef00a1b85ef8814
skukucka98 <skukucka98@gmail.com>
2025-04-24 17:55:26 -0400

    deploying site to github

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from block_markdown import markdown_to_html_node, extract_title
 from inline_markdown import *
 
@@ -21,7 +22,7 @@ def copy_dir(from_path, dest_path):
             shutil.copy(s, d)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = ""
     with open(from_path, "r") as f:
@@ -34,6 +35,7 @@ def generate_page(from_path, template_path, dest_path):
     content_html = markdown_to_html_node(markdown).to_html()
     content_title = extract_title(markdown)
     generated_html = template_html.replace("{{ Title }}", content_title).replace("{{ Content }}", content_html)
+    generated_html = generated_html.replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
@@ -41,7 +43,7 @@ def generate_page(from_path, template_path, dest_path):
         f.write(generated_html)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.makedirs(dest_dir_path)
     for item in os.listdir(dir_path_content):
@@ -50,13 +52,18 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         print(f"{s} -> {d}")
         
         if os.path.isdir(s):
-            generate_pages_recursive(s, template_path, d)
+            generate_pages_recursive(s, template_path, d, basepath)
         else:
-            generate_page(s, template_path, d.replace(".md", ".html"))
+            generate_page(s, template_path, d.replace(".md", ".html"), basepath)
     
 
 def main():
-    copy_dir("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[0]
+    if not basepath:
+        basepath = "/"
+    print(f"basepath set to: {basepath}")
+
+    copy_dir("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
